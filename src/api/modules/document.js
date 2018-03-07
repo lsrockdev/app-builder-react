e412257a989687eb03b8f818811475b622dd90f7
rockdev <19998916+rockdev101@users.noreply.github.com>
2018-03-07 18:29:49 -0500

    actions/sagas for document preview

--- src/api/modules/document.js
+++ src/api/modules/document.js
@@ -5,22 +5,27 @@ import { requestSuccess, requestFail } from 'utils/request';
 // Constants
 // ------------------------------------
 export const GET_DOCUMENTS = 'GET_DOCUMENTS';
+export const GET_DOCUMENT = 'GET_DOCUMENT';
 export const CREATE_DOCUMENT = 'CREATE_DOCUMENT';
 export const UPDATE_DOCUMENT = 'UPDATE_DOCUMENT';
 export const DELETE_DOCUMENT = 'DELETE_DOCUMENT';
 export const SEARCH_DOCUMENT = 'SEARCH_DOCUMENT';
+export const UPDATE_SETTINGS = 'UPDATE_SETTINGS';
 
 // ------------------------------------
 // Actions
 // ------------------------------------
 
 export const getDocuments   = createAction(GET_DOCUMENTS);
+export const getDocument    = createAction(GET_DOCUMENT);
 export const createDocument = createAction(CREATE_DOCUMENT);
 export const updateDocument = createAction(UPDATE_DOCUMENT);
 export const deleteDocument = createAction(DELETE_DOCUMENT);
 export const searchDocument = createAction(SEARCH_DOCUMENT);
+export const updateSettings = createAction(UPDATE_SETTINGS);
 
 const initialState = {
+  document: {},
   documents: [],
   status: 'INIT',
   error: null,
@@ -40,6 +45,18 @@ export default handleActions(
     [requestFail(GET_DOCUMENTS)]: (state, { payload }) => ({
       ...state,
       status: requestFail(GET_DOCUMENTS),
+      error: payload,
+    }),
+
+    [requestSuccess(GET_DOCUMENT)]: (state, { payload }) => ({
+      ...state,
+      document: payload,
+      status: requestSuccess(GET_DOCUMENT),
+    }),
+
+    [requestFail(GET_DOCUMENT)]: (state, { payload }) => ({
+      ...state,
+      status: requestFail(GET_DOCUMENT),
       error: payload,
     }),
 
@@ -85,6 +102,17 @@ export default handleActions(
     [requestFail(DELETE_DOCUMENT)]: (state, { payload }) => ({
       ...state,
       status: requestFail(DELETE_DOCUMENT),
+      error: payload,
+    }),
+
+    [requestSuccess(UPDATE_SETTINGS)]: (state, { payload }) => ({
+      ...state,
+      status: requestSuccess(UPDATE_SETTINGS),
+    }),
+
+    [requestFail(UPDATE_SETTINGS)]: (state, { payload }) => ({
+      ...state,
+      status: requestFail(UPDATE_SETTINGS),
       error: payload,
     }),
 
--- src/api/sagas/document.js
+++ src/api/sagas/document.js
@@ -1,10 +1,12 @@
 import { takeLatest, takeEvery, call } from 'redux-saga/effects';
 import {
   GET_DOCUMENTS,
+  GET_DOCUMENT,
   CREATE_DOCUMENT,
   UPDATE_DOCUMENT,
   DELETE_DOCUMENT,
   SEARCH_DOCUMENT,
+  UPDATE_SETTINGS,
 } from 'api/modules/document';
 
 import request from 'utils/request';
@@ -14,6 +16,15 @@ const getDocuments = request({
   method: 'GET',
   url: 'my/documents',
 });
+
+const getDocument = function* (action) {
+  const apiRequest = request({
+    type: GET_DOCUMENT,
+    method: 'GET',
+    url: 'document/' + action.payload.id,
+  });
+  yield call(apiRequest, action.payload);
+}
 
 const createDocument = request({
   type: CREATE_DOCUMENT,
@@ -46,10 +57,21 @@ const searchDocument = function* (action) {
   yield call(apiRequest, action.payload)
 };
 
+const updateSettings = function* (action) {
+  const apiRequest = request({
+    type: UPDATE_SETTINGS,
+    method: 'PUT',
+    url: 'document/' + action.payload.id + '/settings',
+  });
+  yield call(apiRequest, action.payload)
+};
+
 export default function* rootSaga() {
   yield takeLatest(GET_DOCUMENTS, getDocuments);
+  yield takeLatest(GET_DOCUMENT, getDocument);
   yield takeLatest(SEARCH_DOCUMENT, searchDocument);
   yield takeEvery(CREATE_DOCUMENT, createDocument);
   yield takeEvery(UPDATE_DOCUMENT, updateDocument);
   yield takeEvery(DELETE_DOCUMENT, deleteDocument);
+  yield takeEvery(UPDATE_SETTINGS, updateSettings);
 }
