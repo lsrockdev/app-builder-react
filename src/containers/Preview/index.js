f16553f6c18fe7f6f13c3a432e0eb9dfdf6c3564
rockdev <19998916+rockdev101@users.noreply.github.com>
2018-03-09 16:55:40 -0500

    help link

--- src/containers/Preview/index.js
+++ src/containers/Preview/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import Header from 'components/Header'
 import { connect } from 'react-redux'
@@ -206,7 +207,7 @@ class Preview extends Component {
       <div className="viewport vbox">
         <Header showBuilder={true}/>
         {this.renderMainContent()}
-        <button className="general-help-button">?</button>
+        <Link to="/support" className="general-help-button">?</Link>
       </div>
     )
   }
