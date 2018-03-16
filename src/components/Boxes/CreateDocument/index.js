b2548e830d39b603341762716e75557a54e8f82d
rockdev <19998916+rockdev101@users.noreply.github.com>
2018-03-16 02:09:18 +0200

    create document button

--- src/components/Boxes/CreateDocument/index.js
+++ src/components/Boxes/CreateDocument/index.js
@@ -9,11 +9,11 @@ const createDocument = (props) => {
             src="https://d1xvn5mjulg4qv.cloudfront.net/3.0.0/images/document.png"
             className="box-img"
             alt="create-document" />
-          <div
+          <button
             className="create-document"
             onClick={props.openDocumentModal}>
             Create Document
-          </div>
+          </button>
           <div className="box-footer">
             <NavLink className="help" to="/support" >Need Help?</NavLink>
           </div>
