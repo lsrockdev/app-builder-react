46c96c4877ae2897639771bdc62442750bf95f9c
rockdev <19998916+rockdev101@users.noreply.github.com>
2018-03-18 23:15:47 +0000

    Added missing builder route

--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,59 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Library from 'containers/Library';
-import Documents from 'containers/Documents';
-import Preview from 'containers/Preview';
-import Dashboard from 'containers/Dashboard';
-import LogIn from 'containers/LogIn';
-import SignUp from 'containers/SignUp';
-import RecoverPassword from 'containers/RecoverPassword';
-import NewPassword from 'containers/NewPassword';
-import Homepage from 'containers/Homepage/index.js';
-import { userIsAuthenticatedRedir } from 'utils/authHelper';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Library from "containers/Library";
+import Documents from "containers/Documents";
+import Builder from "containers/Builder";
+import Preview from "containers/Preview";
+import Dashboard from "containers/Dashboard";
+import LogIn from "containers/LogIn";
+import SignUp from "containers/SignUp";
+import RecoverPassword from "containers/RecoverPassword";
+import NewPassword from "containers/NewPassword";
+import Homepage from "containers/Homepage/index.js";
+import { userIsAuthenticatedRedir } from "utils/authHelper";
 
 const rootRoute = () => (
   <div>
-      {console.log()}
-      {window.location.href.indexOf('token') > -1 &&              
-        <Route exact path="/" component={NewPassword} />}
+    {console.log()}
+    {window.location.href.indexOf("token") > -1 && (
+      <Route exact path="/" component={NewPassword} />
+    )}
 
-      {window.location.href.indexOf('token') === -1 &&    
-        <Route exact path="/" component={Homepage} />}
+    {window.location.href.indexOf("token") === -1 && (
+      <Route exact path="/" component={Homepage} />
+    )}
   </div>
-)
+);
 
 const Routes = () => (
   <Router>
-    <Switch>          
-      <Route exact path="/" component={rootRoute}/>
-      <Route exact path="/library" component={userIsAuthenticatedRedir(Library)} />
-      <Route exact path="/documents" component={userIsAuthenticatedRedir(Documents)} />
-      <Route exact path="/preview/:documentId" component={userIsAuthenticatedRedir(Preview)} />
-      <Route exact path="/support" component={userIsAuthenticatedRedir(Dashboard)} />
+    <Switch>
+      <Route exact path="/" component={rootRoute} />
+      <Route
+        exact
+        path="/builder/:documentId"
+        component={userIsAuthenticatedRedir(Builder)}
+      />
+      <Route
+        exact
+        path="/library"
+        component={userIsAuthenticatedRedir(Library)}
+      />
+      <Route
+        exact
+        path="/documents"
+        component={userIsAuthenticatedRedir(Documents)}
+      />
+      <Route
+        exact
+        path="/preview/:documentId"
+        component={userIsAuthenticatedRedir(Preview)}
+      />
+      <Route
+        exact
+        path="/support"
+        component={userIsAuthenticatedRedir(Dashboard)}
+      />
       <Route exact path="/login" component={LogIn} />
       <Route exact path="/sign-up" component={SignUp} />
       <Route exact path="/recover/password" component={RecoverPassword} />
